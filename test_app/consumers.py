bf793ccbe67cefa7700715d3f2d56a98d2593c16
Nija P <nijap@techversantinfotech.com>
2018-05-29 16:48:26 +0530

    first commit

--- test_app/consumers.py
+++ test_app/consumers.py
@@ -9,8 +9,8 @@ class TestWebsocketView(JsonWebsocketConsumer):
         """
         Called with decoded JSON content.
         """
-        direction = content.get('direction', 'all')
-        self.send_json(content={"test":123})
+        direction = content.get('test', '123')
+        self.send_json(content={"test": direction})
         while True:
             time.sleep(10)
-            self.send_json(content={"test":123677})
+            self.send_json(content={"test": direction})
